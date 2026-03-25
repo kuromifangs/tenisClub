95399283cf2d2a4fc388759478f11045aaeb4f9e
kuromifangs <158347555+kuromifangs@users.noreply.github.com>
2026-03-25 19:32:53 +0100

    Add files via upload

---
+++ tenisClub/src/test/java/com/tenisclub/tenisclub/TenisclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenisclub.tenisclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
