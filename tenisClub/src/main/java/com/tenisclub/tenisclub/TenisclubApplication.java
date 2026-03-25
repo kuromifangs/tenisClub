2e34bbe6c0f2a2f82879445e24cc31d242f8620b
kuromifangs <158347555+kuromifangs@users.noreply.github.com>
2026-03-25 19:29:17 +0100

    Add files via upload

---
+++ tenisClub/src/main/java/com/tenisclub/tenisclub/TenisclubApplication.java
@@ -0,0 +1,13 @@
+package com.tenisclub.tenisclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisclubApplication.class, args);
+	}
+
+}
