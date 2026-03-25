936e77ba9f90661241b729cec44ecd3db4f8699f
Louiss01 <luisyfrancisco@gmail.com>
2026-03-25 20:06:07 +0100

    Prueba import

--- tenisClub/src/main/java/com/tenisclub/tenisclub/TenisclubApplication.java
+++ tenisClub/src/main/java/com/tenisclub/tenisclub/TenisclubApplication.java
@@ -1,7 +1,7 @@
 package com.tenisclub.tenisclub;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Importar framework
 
 @SpringBootApplication
 public class TenisclubApplication {
